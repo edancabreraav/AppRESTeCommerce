40fa23a0793c48e37db943f6982943a6572470f7
cabrera <edancabreraav@ittepic.edu.mx>
2024-11-26 00:12:20 -0700

    API completa, creo

--- src/api/v1/routes/prodServ.routes.js
+++ src/api/v1/routes/prodServ.routes.js
@@ -24,20 +24,14 @@ router.post('/presentaciones/subdocs/:id/:idPresentacion/:seccionPresentacion', 
 //----------PUT----------
 //Ruta para modificar un producto
 router.put("/:id", prodServController.putProdServItem);
-
 //Ruta para modificar los campos principales {IdProdServOK, IdProdServBK, CodigoBarras, DesProdServ, Indice} sin afectar los campos de subdocumentos {estatus, presentaciones, info_ad}
 router.put('/update/:id', prodServController.putPrimaryProdServ)
 
 //SUBDOCUMENTOS
 //Ruta para modificar un subdocumento a una sección de producto
-router.put(
-  "/:id/:seccion/:idSeccion",
-  prodServController.putProdServSubdocument
-);
-
+router.put("/:id/:seccion/:idSeccion", prodServController.putProdServSubdocument);
 //Ruta para modificar el subdocumento PRESENTACIONES sin afectar sus subdocumentos internos {estatus[], info_vta[], archivos[]}
 router.put('/presentaciones/primary/:id/:idPresentacion', prodServController.putPrimaryPresentacion);
-
 //Ruta para modificar un subdocumento (estatus[], info_vta[], archivos[]) que se encuentre dentro de un subdocumento presentaciones
 router.put('/presentaciones/subdocs/:id/:idPresentacion/:seccionPresentacion/:idSeccionPresentacion', prodServController.putPresentacionSubdocument);
 
